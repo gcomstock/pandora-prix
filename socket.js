36928e2e78d5ee1338841df123c24026b6c3da10
Marc Novakowski <marc@pandora.com>
2015-05-26 16:32:47 -0700

    more websocket client

--- socket.js
+++ socket.js
@@ -1,4 +1,13 @@
 
 function start() {
+	var socket = new WebSocket("ws://localhost:8080/racing");
 	
+	socket.onopen = function(event) {
+		socket.send("HELLO");
+	}
+	
+	socket.onmessage = function(event) {
+		console.log(event.data);
+	}
+
 }
